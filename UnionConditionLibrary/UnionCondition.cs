02be281a773deac9cdd1dd72f3d72b63841b384a
Bill Chung <bill_chungiii@hotmail.com>
2019-09-04 23:57:22 +0800

    data 改名為 source

--- UnionConditionLibrary/UnionCondition.cs
+++ UnionConditionLibrary/UnionCondition.cs
@@ -14,10 +14,10 @@ namespace UnionConditionLibrary
     public static class UnionCondition
     {
 
-        public static bool NeedExecute<TCondition, TSource>(this IEnumerable<ConditionExpression<TCondition, TSource>> expressions, TCondition condition, TSource data)
+        public static bool NeedExecute<TCondition, TSource>(this IEnumerable<ConditionExpression<TCondition, TSource>> expressions, TCondition condition, TSource source)
         {         
 
-            return expressions.All((y) => !y.Precondition(condition) ||  y.Postcondition(data));
+            return expressions.All((y) => !y.Precondition(condition) ||  y.Postcondition(source));
         }
     
     }
