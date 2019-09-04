ef34ea182cd780fdbe5f3c9790c5e8dc7e052993
Bill Chung <bill_chungiii@hotmail.com>
2019-09-05 02:03:10 +0800

    重新排版

--- IntersectLibrary/Intersection.cs
+++ IntersectLibrary/Intersection.cs
@@ -19,7 +19,7 @@ namespace IntersectLibrary
 
         private static IEnumerable<IEnumerable<T2>> InnerExecute<T1, T2>(this IEnumerable<T2> source, IEnumerable<ConditionExpression<T1, T2>> expressions, T1 condition)
         {
-          return   expressions.Where((x) => x.Precondition(condition)).Select((y) => source.Where (y.Postcondition));            
+            return expressions.Where((x) => x.Precondition(condition)).Select((y) => source.Where(y.Postcondition));
         }
     }
 }
